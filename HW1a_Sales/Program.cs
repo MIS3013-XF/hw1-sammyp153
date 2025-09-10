a8beee6293f90d7e4eca4639b69fa93b29bb6b76
Sam Pickens <Samuel.D.Pickens-1@ou.edu>
2025-09-10 10:52:18 -0500

    test

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Sam Pickens
+// Did you seek help ? If yes, specify the helper or web link here: Referenced Sum of 3 In Class Practice
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,38 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            double number;
+            double price;
+
+            double subtotal;
+            double salestax;
+            double total;
+
+            const double TaxRate = 0.085;
+
+            string productAsString;
+            string numberAsString;
+            string priceAsString;
+
+            Console.WriteLine("What is the name of the product you are buying? ");
+            productAsString = Console.ReadLine();
+            Console.WriteLine("How many " + productAsString + "s do you want to buy?");
+            numberAsString = Console.ReadLine();
+            Console.WriteLine("What is the price for each " + productAsString + "?");
+            priceAsString = Console.ReadLine();
+
+            number = Convert.ToDouble(numberAsString);
+            price = Convert.ToDouble(priceAsString);
+
+            subtotal = price * number;
+            salestax = subtotal * TaxRate;
+            total = subtotal + salestax;
+
+            Console.WriteLine("     Your Subtotal is $" + subtotal.ToString("N2"));
+            Console.WriteLine("     Your Sales Tax is $" + salestax.ToString("N2"));
+            Console.WriteLine("     Your Total is $" + total.ToString("N2"));
+
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1b Grade
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Sam Pickens
+// Did you seek help ? If yes, specify the helper or web link here: Referenced Sum of 3 In Class Practice
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,59 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            double Exam1;
+            double Exam2;
+            double Exam3;
+            double Homework;
+            double Participation;
+
+            double Overall;
+
+            string FirstNameAsString;
+            string LastNameAsString;
+            string IDAsString;
+            string Exam1AsString;
+            string Exam2AsString;
+            string Exam3AsString;
+            string HomeworkAsString;
+            string ParticipationAsString;
+
+            const double Exam1Percent = 0.15;
+            const double Exam2Percent = 0.25;
+            const double Exam3Percent = 0.25;
+            const double HomeworkPercent = 0.20;
+            const double ParticipationPercent = .15;
+
+            Console.WriteLine("What is your First Name?");
+            FirstNameAsString = Console.ReadLine();
+            Console.WriteLine("What is your Last Name?");
+            LastNameAsString = Console.ReadLine();
+            Console.WriteLine("What is your student ID number?");
+            IDAsString = Console.ReadLine();
+            Console.WriteLine("What is your Exam 1 grade?");
+            Exam1AsString = Console.ReadLine();
+            Console.WriteLine("What is your Exam 2 grade?");
+            Exam2AsString = Console.ReadLine();
+            Console.WriteLine("What is your Exam 3 grade?");
+            Exam3AsString = Console.ReadLine();
+            Console.WriteLine("What is your Homework overall grade?");
+            HomeworkAsString = Console.ReadLine();
+            Console.WriteLine("What is your Participation overall grade?");
+            ParticipationAsString = Console.ReadLine();
+
+            Exam1 = Convert.ToDouble(Exam1AsString);
+            Exam2 = Convert.ToDouble(Exam2AsString);
+            Exam3 = Convert.ToDouble(Exam3AsString);
+            Homework = Convert.ToDouble(HomeworkAsString);
+            Participation = Convert.ToDouble(ParticipationAsString);
+
+            Overall = (Exam1 * Exam1Percent) + (Exam2 * Exam2Percent) + (Exam3 * Exam3Percent) 
+                + (Homework * HomeworkPercent) + (Participation * ParticipationPercent);
+
+            Console.WriteLine(FirstNameAsString + " " + LastNameAsString + " (" + IDAsString + 
+                "), your overall grade is " + Overall.ToString("N2") + "%");
+
+
         }
     }
 }
